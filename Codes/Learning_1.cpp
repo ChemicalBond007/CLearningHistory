191327d9abd6344e647ef387210570e3ad6ad5fd
ChemicalBond007 <ChemicalBond007@protonmail.com>
2025-07-28 05:07:36 +0000

    Restore To Basic Status

--- Codes/Learning_1.cpp
+++ Codes/Learning_1.cpp
@@ -6,28 +6,9 @@
 #include<ctime>
 using namespace std;
 
-int randomNUMBER();
-void function(int input);
-
-int functionvar=randomNUMBER();
 
 int main()
 {
-    extern int functionvar;
-    function(0);
-    int var1=1;
-    var1+=1;
-    const int var2=var1;
-    printf("\n11\n%d\n",functionvar);
+    int a;
     return 0;
-}
-void function(int input)
-{
-    extern int functionvar;
-    printf("The variable given to the function is %d.\nWhile the external var is %d", input, functionvar);
-}
-int randomNUMBER()
-{
-    srand(time(NULL));
-    return rand();
 }
