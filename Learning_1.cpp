8c11fb778a0c0919b66683e2bd67c6a1aeadb9be
ChemicalBond007 <ChemicalBond007@protonmail.com>
2025-08-25 09:26:16 +0000

    Keyword Learning_1

--- Learning_1.cpp
+++ Learning_1.cpp
@@ -1,14 +1,13 @@
-//#include<bits/cpp.h>
-#include<iostream>
-#include<cstdlib>
-#include<cstring>
-#include<cstdio>
-#include<ctime>
-using namespace std;
+#include <stdio.h>
+#include <assert.h> // For the friendlier static_assert
+#include <stdint.h> // For uintptr_t
 
+int main() {
+    // This check happens at compile time.
+    // If you compile this on a 32-bit system where pointers are 4 bytes,
+    // the compilation will fail with the error message.
+    static_assert(sizeof(void*) == 8, "This code requires a 64-bit architecture.");
 
-int main()
-{
-    int a;
+    printf("Successfully compiled on a 64-bit system.\n");
     return 0;
 }
