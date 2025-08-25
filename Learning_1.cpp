cd5326fc89463d5b2e4c50161ad0be489f05ff7e
ChemicalBond007 <ChemicalBond007@protonmail.com>
2025-08-25 13:37:06 +0000

    regularisation

--- Learning_1.cpp
+++ Learning_1.cpp
@@ -1,14 +1,15 @@
 #include <stdio.h>
+#include <string.h>
 #include <float.h>
  
 void printconst(char* input)
 {
-   const char *strings=input;
+   const char *strings=strcat(input,"\n");
    printf(strings);
 }
 int main()
 {
-   char * in[50];
+   char in[50];
    scanf("%s",in);
    printconst((char*)in);
    return 0;
