65fa5daf2146c3313a5d24410107251dceb36657
ChemicalBond007 <ChemicalBond007@protonmail.com>
2025-08-30 17:49:19 +0800

    Trial

--- Learning_1.cpp
+++ Learning_1.cpp
@@ -1,11 +1,14 @@
 #include <stdio.h>
- 
+#include <time.h>
+int max(int input_1, int input_2)
+{
+   return input_1>input_2?input_1:input_2;
+}
 int main ()
 {
-   int a = 10;
-   int * ptra=&a;
-   printf("The location of a is %p.\n", ptra);
-   *ptra=++a;
-   printf("The location of ++a is %p.\n", ptra);
+   int a=10,b=20;
+   for(int i=1;i<=100;i++)
+      int v=1;
+   printf("%d\nTime consumed is %d\n",max(a,b),clock()-1200);
    return 0;
 }
